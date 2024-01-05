ff3de5383769e55e7e5f24252546035ee4a9c1b9
Aarif Khan <aarifkhan7896@gmail.com>
2024-01-05 18:09:14 +0530

    split form into child components with formBuilder and form group directive

--- src/app/user-notes/add-notes/add-notes.component.ts
+++ src/app/user-notes/add-notes/add-notes.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-notes.component.html',
   styleUrls: ['./add-notes.component.scss'],
 })
-export class AddNotesComponent {}
+export class AddNotesComponent {
+  @Input() formGroupName!: string;
+
+  notesForm!: FormGroup;
+
+  constructor(private readonly rootFormGroup: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    this.notesForm = this.rootFormGroup.control.get(
+      this.formGroupName
+    ) as FormGroup;
+  }
+}
--- src/app/user-notes/user-notes.component.ts
+++ src/app/user-notes/user-notes.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddNotesComponent } from './add-notes/add-notes.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-notes',
   standalone: true,
-  imports: [CommonModule, AddNotesComponent],
+  imports: [CommonModule, AddNotesComponent, ReactiveFormsModule],
   templateUrl: './user-notes.component.html',
   styleUrls: ['./user-notes.component.scss'],
 })
-export class UserNotesComponent {}
+export class UserNotesComponent {
+  notesForm: FormGroup = this.createForm();
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      addNotes: this.fb.group({
+        title: [],
+        description: [],
+      }),
+    });
+  }
+}
